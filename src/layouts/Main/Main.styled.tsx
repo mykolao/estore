e8314c3c5cc8b595e57169b373aaf9ce5acb6f1b
mykolao <mykolaoleskiv@gmail.com>
2022-07-07 16:05:12 +0300

    style: Main wrapper

--- src/layouts/Main/Main.styled.tsx
+++ src/layouts/Main/Main.styled.tsx
@@ -6,4 +6,9 @@ export const MainStyled = styled.main`
   z-index: 0;
   flex: 1;
   margin-top: ${({ theme: { hHeader } }) => hHeader};
+
+  & .wrapper {
+    width: ${({ theme: { wApp } }) => wApp};
+    padding: 0 18px;
+  }
 `;
