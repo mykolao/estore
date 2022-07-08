cde7f625e3ab394b9e2c183941f1395083cc2f11
mykolao <mykolaoleskiv@gmail.com>
2022-07-08 14:31:06 +0300

    add theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,4 +3,6 @@ export const theme = {
   hHeader: '80px',
   cBlack: 'hsla(216, 8%, 12%, 1)',
   cWhite: 'hsla(0, 0%, 100%, 1)',
+  cPrimary: 'hsla(136, 53%, 59%, 1)',
+  cGrey: 'hsla(231, 6%, 58%, 1)',
 } as const;
