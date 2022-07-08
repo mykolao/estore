f994fd70daad7bbb0af007a470b2fc51d4caa97f
mykolao <mykolaoleskiv@gmail.com>
2022-07-09 01:11:11 +0300

    add CategorySelect

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,16 +1,16 @@
 import { PureComponent } from 'react';
 
+import CategorySelect from 'components/CategorySelect';
 import Logo from 'components/Logo';
 import Actionbar from 'layouts/Actionbar';
 import { HeaderStyled } from 'layouts/Header/Header.styled';
-import Nav from 'layouts/Nav';
 
 export class Header extends PureComponent {
   render() {
     return (
       <HeaderStyled>
         <div className="wrapper">
-          <Nav />
+          <CategorySelect />
           <Logo />
           <Actionbar />
         </div>
