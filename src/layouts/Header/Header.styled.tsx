6f1ed86eaf59c9ebc787b98c4c3353cd30c4d60b
mykolao <mykolaoleskiv@gmail.com>
2022-07-09 01:10:44 +0300

    style: background

--- src/layouts/Header/Header.styled.tsx
+++ src/layouts/Header/Header.styled.tsx
@@ -6,6 +6,7 @@ export const HeaderStyled = styled.header`
   justify-content: center;
   z-index: 10;
   height: ${({ theme: { hHeader } }) => hHeader};
+  background-color: ${({ theme: { cWhite } }) => cWhite};
   width: 100vw;
 
   & .wrapper {
