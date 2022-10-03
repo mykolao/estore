b35f7bf26c821a3921ccd688966de4a1e0525b65
mykolao <mykolaoleskiv@gmail.com>
2022-10-03 22:31:13 +0300

    feat: dispatch initCart thunk

--- src/store/app/thunks/initApp.ts
+++ src/store/app/thunks/initApp.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { queries } from 'apollo/queries';
 import { appActions } from 'store/app/slice';
+import { cartThunks } from 'store/cart/thunks';
 import { categoryThunks } from 'store/category';
 import { currencyThunks } from 'store/currency';
 import { ThunkApi } from 'store/types';
@@ -10,6 +11,7 @@ const typePrefix = 'Initialize App';
 const { setLoading, setError } = appActions;
 const { initCategory } = categoryThunks;
 const { initCurrency } = currencyThunks;
+const { initCart } = cartThunks;
 const { initialQuery } = queries;
 
 export const initApp = createAsyncThunk<void, void, ThunkApi>(
@@ -21,6 +23,7 @@ export const initApp = createAsyncThunk<void, void, ThunkApi>(
       } = await initialQuery();
       dispatch(initCategory(categories.map(({ name }) => name)));
       dispatch(initCurrency(currencies));
+      dispatch(initCart());
       dispatch(setLoading(false));
     } catch {
       dispatch(setError(true));
