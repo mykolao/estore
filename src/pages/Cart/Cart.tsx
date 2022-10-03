22586c362b171c601a43c489a0ca970b02641dc7
mykolao <mykolaoleskiv@gmail.com>
2022-10-03 22:38:39 +0300

    feat: Cart page

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,9 +1,18 @@
 import { PureComponent } from 'react';
 
+import CartSummary from 'components/CartSummary';
+import CartTitle from 'components/CartTitle';
+import CartItemList from 'layouts/CartItemList';
 import Main from 'layouts/Main';
 
 export class Cart extends PureComponent {
   render() {
-    return <Main>Cart Page</Main>;
+    return (
+      <Main>
+        <CartTitle />
+        <CartItemList />
+        <CartSummary />
+      </Main>
+    );
   }
 }
