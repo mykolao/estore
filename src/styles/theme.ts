822a898e85d3547801b4b1510b7630f30f228ecd
mykolao <mykolaoleskiv@gmail.com>
2022-10-04 23:28:18 +0300

    style: add backdrop color to the theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,4 +5,5 @@ export const theme = {
   cWhite: 'hsla(0, 0%, 100%, 1)',
   cPrimary: 'hsla(136, 53%, 59%, 1)',
   cGrey: 'hsla(231, 6%, 58%, 1)',
+  cBackdrop: 'hsla(247.059, 13%, 25%, 0.22)',
 } as const;
