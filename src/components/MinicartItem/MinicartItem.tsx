52636a20634685dc7e4c7ff77c7b58d00323af04
mykolao <mykolaoleskiv@gmail.com>
2022-10-08 15:55:42 +0300

    feat: redirect to product page on title, price and image click

--- src/components/MinicartItem/MinicartItem.tsx
+++ src/components/MinicartItem/MinicartItem.tsx
@@ -1,34 +1,43 @@
 import { PureComponent } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { MinicartItemStyled } from 'components/MinicartItem/MinicartItem.styled';
 import MinicartItemControls from 'components/MinicartItemControls';
 import MinicartItemDetails from 'components/MinicartItemDetails';
+import { PATHS } from 'routes';
 import { TCartItem } from 'store/cart';
 
 interface Props {
   value: TCartItem;
 }
 
+const { product } = PATHS;
+
 export class MinicartItem extends PureComponent<Props> {
   render() {
     const {
       value: {
-        product: { brand, name, prices, attributes, gallery },
+        product: { brand, name, prices, attributes, gallery, id },
         selectedAttributes,
         quantity,
         itemId,
       },
     } = this.props;
 
-    const detailsProps = { selectedAttributes, attributes, brand, name, prices };
+    const detailsProps = { selectedAttributes, attributes, brand, name, prices, id };
     const img = gallery[0];
+
+    const path = `${product}/${id}`;
 
     return (
       <MinicartItemStyled>
         <MinicartItemDetails {...detailsProps} />
         <div className="rightSection">
           <MinicartItemControls itemId={itemId} numberOfItems={quantity} />
-          <img src={img} alt="product" />
+          <Link to={path}>
+            <img src={img} alt="product" />
+          </Link>
         </div>
       </MinicartItemStyled>
     );
--- src/components/MinicartItemDetails/MinicartItemDetails.tsx
+++ src/components/MinicartItemDetails/MinicartItemDetails.tsx
@@ -1,28 +1,35 @@
 import { PureComponent } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { TAttributeSet, TProduct } from 'apollo/types';
 import CurrentPrice from 'components/CurrentPrice';
 import MinicartAttributes from 'components/MinicartItemDetails/MinicartAttributes';
 import { MinicartItemDetailsStyled } from 'components/MinicartItemDetails/MinicartItemDetails.styled';
+import { PATHS } from 'routes';
 
-interface Props extends Pick<TProduct, 'brand' | 'name' | 'prices' | 'attributes'> {
+interface Props
+  extends Pick<TProduct, 'brand' | 'name' | 'prices' | 'attributes' | 'id'> {
   selectedAttributes: TAttributeSet[];
 }
 
+const { product } = PATHS;
+
 export class MinicartItemDetails extends PureComponent<Props> {
   render() {
-    const { brand, name, prices, attributes, selectedAttributes } = this.props;
+    const { id, brand, name, prices, attributes, selectedAttributes } = this.props;
+    const path = `${product}/${id}`;
 
     return (
       <MinicartItemDetailsStyled>
-        <div className="title">
+        <Link className="title" to={path}>
           {brand}
           <br />
           {name}
-        </div>
-        <div className="price">
+        </Link>
+        <Link className="price" to={path}>
           <CurrentPrice prices={prices} />
-        </div>
+        </Link>
         <MinicartAttributes
           attributes={attributes}
           selectedAttributes={selectedAttributes}
